0effcbff72e112d675215f43424a77e613292d7a
groundP7 <p.ground714@gmail.com>
2025-02-14 11:14:03 +0900

    sdfasd

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ from huggingface_hub import snapshot_download
 
 def get_huggingface_token() :
 
-    token = st.secrets.get("HUGGINGFACE_API_TOKEN")
+    # 실서버에서는 os의 환경변수에 셋팅된다. 따라서 환경변수 읽어오는 코드로
+    # 작성해야된다
+    token = os.environ.get("HUGGINGFACE_API_TOKEN")
+
+    # 토은이 환경변수에 없으면, 로컬에서 동작하니까 로컬에서 읽어오도록 한다.
+    if token is None :
+        token = st.secrets.get("HUGGINGFACE_API_TOKEN")
     return token
 
 @st.cache_resource
